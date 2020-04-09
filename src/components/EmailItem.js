9b897d17403420fda098d7f54ca12dd7550b2b68
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2020-04-09 16:45:42 +0200

    Turn EmailItem into a stateful component (class)

--- src/components/EmailItem.js
+++ src/components/EmailItem.js
@@ -2,24 +2,50 @@ import React from 'react';
 
 
 
-function EmailItem(props) {
-  return (
-    <div className="email-item email-item-selected pure-g">
-      <div className="pure-u">
-        <img width="64" height="64" alt={`${props.name}&#x27;s avatar`} className="email-avatar" src={props.image} />
-      </div>
+class EmailItem extends React.Component {
 
-      <div className="pure-u-3-4">
-        <h5 className="email-name">{props.name}</h5>
-        <h4 className="email-subject">{props.subject}</h4>
-        <p className="email-desc">
-          {props.content}
-        </p>
+  constructor(props) {
+    super(props);
+    this.state = {
+      read: false,
+      fakeRead: true
+    };
+    this.mailRead = this.mailRead.bind(this);
+  }
+
+  // methode()
+  mailRead() {
+    // BAD if you are not in the constructor
+    // this.state = { read: true };
+    // GOOD
+    this.setState({
+      read: true
+    })
+  }
+
+  render() {
+    const divClassName = this.state.read
+      ? "email-item pure-g"
+      : "email-item email-item-unread pure-g";
+    return (
+      <div className={divClassName} onClick={this.mailRead}>
+        <div className="pure-u">
+          <img width="64" height="64" alt={`${this.props.name}&#x27;s avatar`} className="email-avatar" src={this.props.image} />
+        </div>
+
+        <div className="pure-u-3-4">
+          <h5 className="email-name">{this.props.name}</h5>
+          <h4 className="email-subject">{this.props.subject}</h4>
+          <p className="email-desc">
+            {this.props.content}
+          </p>
+        </div>
       </div>
-    </div>
-  )
+    )
+
+  }
+
 }
-
 export default EmailItem;
 
 //Hello from Toronto
